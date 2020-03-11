95fb20d26145ab4dfa322b3ad780942ecd4ca91a
Afterlife-dev <58752037+Afterlife-dev@users.noreply.github.com>
2020-03-11 19:31:09 +0200

    Добавьте файлы проекта.

---
+++ CSharpAdvanced_ITEA_2020/SmallData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson_1
+{
+    class SmallData
+    {
+        public SmallData something;
+
+        private int BigBob;
+
+        protected SmallData Something
+        {
+            get
+            {
+                return something;
+            }
+            private set
+            {
+                if (value.BigBob > 1)
+                {
+                    something = value;
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+            }
+        }
+    }
+
+
+}
---
+++ Lesson_1/SmallData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson_1
+{
+    class SmallData
+    {
+        public SmallData something;
+
+        private int ProgVersion;
+
+        protected SmallData Something
+        {
+            get
+            {
+                return something;
+            }
+            private set
+            {
+                if (ProgVersion > 2)
+                {
+                    something = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+
+        public SmallData(int progVers)
+        {
+            ProgVersion = progVers;
+        }
+
+
+
+    }
+}
